6502b73a6fe7646e119ba8f6e37c24a6374df430
qiqi <kdyq@vip.qq.com>
2017-04-12 21:02:59 +0800

    save

---
+++ config.py
@@ -0,0 +1,15 @@
+#-*- coding:utf8 -*-
+__author__ = 'qiqi'
+
+#MYSQL
+SQLALCHEMY_DATABASE_URI = 'mysql://root:sdfsdf@127.0.0.1:3306/qblog'
+SQLALCHEMY_ENCODING = "utf-8"
+SQLALCHEMY_ECHO = False
+DEBUG = True
+
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+#SERVER PORT
+SERVER_PORT = 5000
+
+SECRET_KEY = "sdfdsf"
